515461c1d283ffcd7626ecd542db1e792774be11
Elhadji Massow <massow.ndiaye3011@gmail.com>
2026-02-18 13:46:31 -0500

    Fixing text case sensitive

--- client/src/loaders/footerLoader.jsx
+++ client/src/loaders/footerLoader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Footer from "./components/Footer";
+import Footer from "../components/Footer";
 
 const root = document.getElementById("footer-root");
 if (root) ReactDOM.createRoot(root).render(<Footer />);
--- client/src/loaders/navbarLoader.jsx
+++ client/src/loaders/navbarLoader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 
 const root = document.getElementById("navbar-root");
 if (root) ReactDOM.createRoot(root).render(<Navbar />);
