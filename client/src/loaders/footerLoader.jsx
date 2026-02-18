2c2c302ffd34b9b662be7a31f54c95ce29d56c1e
Elhadji Massow <massow.ndiaye3011@gmail.com>
2026-02-18 13:39:56 -0500

    Testing new import for styling

--- client/src/loaders/footerLoader.jsx
+++ client/src/loaders/footerLoader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Footer from "../components/Footer";
+import "../dist/footer.css";
 
 const root = document.getElementById("footer-root");
 if (root) ReactDOM.createRoot(root).render(<Footer />);
--- client/src/loaders/navbarLoader.jsx
+++ client/src/loaders/navbarLoader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navbar from "../components/Navbar";
+import "../dist/navbar.css";
 
 const root = document.getElementById("navbar-root");
 if (root) ReactDOM.createRoot(root).render(<Navbar />);
