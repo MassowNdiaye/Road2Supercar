e20e9a8be0346ff4a469bdcdd04f92410d1c9f78
Elhadji Massow <massow.ndiaye3011@gmail.com>
2026-02-18 13:41:32 -0500

    Reversing changes

--- client/src/loaders/navbarLoader.jsx
+++ client/src/loaders/navbarLoader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "../dist/navbar.css";
+import Navbar from "./components/Navbar";
 
 const root = document.getElementById("navbar-root");
 if (root) ReactDOM.createRoot(root).render(<Navbar />);
