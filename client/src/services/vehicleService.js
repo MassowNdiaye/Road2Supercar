47259d031bddd9721b836d91be4bc78eab9173a6
Elhadji Massow <massow.ndiaye3011@gmail.com>
2026-02-19 16:46:49 -0500

    Adding Render DB deployment link

--- client/src/services/vehicleService.js
+++ client/src/services/vehicleService.js
@@ -1,6 +1,6 @@
 // Getting Vehicles info from API -> for frontend
 export const getVehicles = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/vehicles`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/api/vehicles`);
 
   if (!response.ok) {
     throw new Error("Failed to fetch vehicles");
